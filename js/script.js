883eade40c8e129a8de17f515afcd9bc1997f779
Manion-sebastian <Sjmanion123@gmail.com>
2022-08-11 22:08:17 -0700

    add content and dom operation. as well as style.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,52 @@
-console.log('Hello, front end');
+
+function toCfromF(initialTemp) {
+    return (initialTemp - 32) * 5/9
+}
+
+function toKfromC(initialTemp) {
+    return initialTemp + 273.15
+}
+
+function toCfromK(initialTemp) {
+    return initialTemp - 273.15
+}
+
+function toFfromC(initialTemp) {
+    return (initialTemp * 9/5) + 32
+}
+
+
+
+
+let convertClick = document.getElementById('start').addEventListener('click', function() {
+    let fromChoice = document.getElementById('fromTemp')
+    let toChoice = document.getElementById('toTemp')
+    let answer = ""
+    let initialTemp = input.value
+
+    if (fromChoice.value === "f" && toChoice.value === "c" ) {
+        answer = toCfromF(initialTemp)
+    } else if (fromChoice.value === 'c' && toChoice.value === 'k') {
+        answer = toKfromc(initialTemp)
+    } else if (fromChoice.value === 'k' && toChoice.value === 'c') {
+        answer = toCfromK(initialTemp)
+    } else if (fromChoice.value === 'c' && toChoice.value === 'f') {
+        answer = toFfromC(initialTemp)
+    } else if (fromChoice.value === 'f' && toChoice.value === 'k') {
+        answer = toKfromC(toCfromF(initialTemp))
+    } else if (fromChoice.value === 'k' && toChoice.value === 'f') {
+        answer = toFfromC(toCfromK(initialTemp))
+    } else {
+        alert("Please select two different systems!")
+    }
+    
+    
+    document.getElementById('output').innerText = answer.toFixed(1) + "°"
+
+})
+
+let clearClick = document.getElementById('end').addEventListener('click', function() {
+    input.value = ""
+    output.value = ""
+    
+})
